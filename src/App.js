5c76cc88b07eab452e87a7d3a2ede1b8fa9c174b
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-03-04 13:58:24 +0530

    updated about us

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { NavLink, Outlet } from "react-router-dom";
-import style from "./App.module.css";
+// import { NavLink, Outlet } from "react-router-dom";
+// import style from "./App.module.css";
+import AboutUs from "./components/LandingPage/AboutUs/AboutUs";
+import HeroPage from "./components/LandingPage/HeroPage/HeroPage";
+import OurServices from "./components/LandingPage/OurServices/OurServices";
 
 function App() {
   return (
     <div className="App">
-      <ul className={`${style["m"]}`}>
+      {/* <ul className={`${style["m"]}`}>
         <div className={`${style["logo"]}`}></div>
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]}`}>For Startups</NavLink></li>
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]}`}>Write a Review</NavLink></li>
@@ -12,8 +15,10 @@ function App() {
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]}`}>FAQ's</NavLink></li>
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]} ${style["m-button-p"]}`}>Join</NavLink></li>
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]} ${style["m-button-s"]}`}>Login</NavLink></li>
-      </ul>
-      <Outlet />
+      </ul> */}
+      <HeroPage/>
+      <AboutUs/>
+      <OurServices/>
     </div>
   );
 }
---
+++ src/components/LandingPage/OurServices/OurServices.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./OurServices.css";
+
+const OurServices = () => {
+  return (
+    <>
+      <div className="m-os">
+        <div className="section-header">
+          <h1 style={{color:"white"}}>Our services</h1>
+        </div>
+        <div className="m-os-c">
+                
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurServices;
