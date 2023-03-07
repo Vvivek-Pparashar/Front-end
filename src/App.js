1a20dea7399ad22f6af2dc3ae9d42d0ccd4734d6
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2023-03-07 12:41:14 +0530

    scroll to top bottom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { NavLink, Outlet } from "react-router-dom";
-// import style from "./App.module.css";
+import "./App.css";
 import AboutUs from "./components/LandingPage/AboutUs/AboutUs";
 import Faqs from "./components/LandingPage/Faq's/Faqs";
 import Footer from "./components/LandingPage/Footer/Footer";
@@ -7,6 +7,7 @@ import HeroPage from "./components/LandingPage/HeroPage/HeroPage";
 import LatestNews from "./components/LandingPage/LatestNews/LatestNews";
 import OurServices from "./components/LandingPage/OurServices/OurServices";
 import StoryTelling from "./components/LandingPage/StoryTelling/StoryTelling";
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
   return (
@@ -20,14 +21,28 @@ function App() {
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]} ${style["m-button-p"]}`}>Join</NavLink></li>
         <li className={`${style["m-item"]}`}><NavLink className={`${style["m-link"]} ${style["m-button-s"]}`}>Login</NavLink></li>
       </ul> */}
-      
-      <HeroPage/>
-      <AboutUs/>
-      <OurServices/>
-      <StoryTelling/>
-      <LatestNews/>
-      <Faqs/>
-      <Footer/>
+
+      <HeroPage />
+      <AboutUs />
+      <OurServices />
+      <StoryTelling />
+      <LatestNews />
+      <Faqs />
+      <Footer />
+      <ScrollToTop
+        smooth
+        id="scroll-to-top"
+        color="white"
+        style={{}}
+        // style={{
+        //   background: "black",
+        //   position: "fixed",
+        //   width: "60px",
+        //   height: "60px",
+        //   right: "20px",
+        //   bottom: "14px",
+        // }}
+      />
     </div>
   );
 }
